4428a38b540e87ec7022ab726828918b8139426c
Nick Gray <nickgray87@gmail.com>
2021-01-14 18:57:08 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,15 @@ def addition (first_number , second_number = 2)
   sum = first_number + second_number
   sum
 end
+def subtraction (first_number , second_number = 2)
+  sum = first_number + second_number
+  sum
+end
+def multiplication (first_number , second_number = 2)
+  sum = first_number + second_number
+  sum
+end
+def division (first_number , second_number = 2)
+  sum = first_number + second_number
+  sum
+end
