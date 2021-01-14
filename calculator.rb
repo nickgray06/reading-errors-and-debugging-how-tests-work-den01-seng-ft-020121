d1710e48557ef57697d22a224e6e5ebc456a15f6
Nick Gray <nickgray87@gmail.com>
2021-01-14 18:59:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,14 +2,17 @@ def addition (first_number = 4 , second_number = 2)
   sum = first_number + second_number
   sum
 end
+
 def subtraction (first_number = 4 , second_number = 2)
   subtraction = first_number - second_number
   subtraction
 end
+
 def multiplication (first_number = 4, second_number = 2)
   product = first_number * second_number
   product
 end
+
 def division (first_number = 4, second_number = 2)
   quotient = first_number / second_number
   quotient
