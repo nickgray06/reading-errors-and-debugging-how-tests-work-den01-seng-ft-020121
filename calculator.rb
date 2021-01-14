89e40a72151cda50d2d96c108db91d7f627a65ac
Nick Gray <nickgray87@gmail.com>
2021-01-14 19:03:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,14 +1,10 @@
 first_number = 4
 second_number = 2
-def addition (first_number = 4 , second_number = 2)
-  sum = first_number + second_number
-  sum
-end
+sum = first_number + second_number
+sum
 
-def subtraction (first_number = 4 , second_number = 2)
-  subtraction = first_number - second_number
-  subtraction
-end
+subtraction = first_number - second_number
+subtraction
 
 def multiplication (first_number = 4, second_number = 2)
   product = first_number * second_number
