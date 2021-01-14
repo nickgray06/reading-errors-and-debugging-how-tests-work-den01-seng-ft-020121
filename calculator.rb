3f0b2d924ba079ae6bc07bd58261ac33089d1786
Nick Gray <nickgray87@gmail.com>
2021-01-14 19:05:00 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
-# Add your variables here
+first_number = 4
+second_number = 2
+sum = first_number + second_number
+sum
+
+difference = first_number - second_number
+difference
+
+product = first_number * second_number
+product
+
+quotient = first_number / second_number
+quotient
