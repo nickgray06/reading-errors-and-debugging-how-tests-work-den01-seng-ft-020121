38cd91568d7be84763c737e3274a2102b739683a
Nick Gray <nickgray87@gmail.com>
2021-01-14 18:59:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,16 +1,16 @@
-def addition (first_number , second_number = 2)
+def addition (first_number = 4 , second_number = 2)
   sum = first_number + second_number
   sum
 end
-def subtraction (first_number , second_number = 2)
+def subtraction (first_number = 4 , second_number = 2)
   subtraction = first_number - second_number
   subtraction
 end
-def multiplication (first_number , second_number = 2)
+def multiplication (first_number = 4, second_number = 2)
   product = first_number * second_number
   product
 end
-def division (first_number , second_number = 2)
+def division (first_number = 4, second_number = 2)
   quotient = first_number / second_number
   quotient
 end
